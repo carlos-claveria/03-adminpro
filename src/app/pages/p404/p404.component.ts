4b0f0da96f0ea657470091f026b6fab297319328
Carlos Claveria <cclaveria@siliconmedia.com>
2021-11-19 22:31:14 +0100

    fin seccion 3 curso de angular-adv

---
+++ src/app/pages/p404/p404.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-p404',
+  templateUrl: './p404.component.html',
+  styleUrls: [ './p404.component.css'  ]
+})
+export class P404Component implements OnInit {
+
+  year = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
